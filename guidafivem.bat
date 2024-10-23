32d66045453c63f573e99b381497c19f62d3939e
danyyctn <youffufmohamed@gmail.com>
2024-10-23 13:18:21 +0200

    Add files via upload

---
+++ guidafivem.bat
@@ -0,0 +1,18 @@
+@echo off
+color a
+echo Benvenuto, questo è un programma realizzato da una bestia di FiveM! Premi <Invio> per vedere il tutorial per vedere come giocare.
+pause >nul
+
+@echo off
+color b
+
+echo 1 - Avere un PC
+echo 2 - Scaricare Steam dal link che si aprirà automaticamente quando premerai <Invio>
+echo 3 - Acquistare GTA
+echo 4 - Scaricare FiveM dal link che si aprirà automaticamente quando premerai <Invio>
+echo 5 - Creare un account Rockstar Games
+echo 6 - Accedi su FiveM col tuo account Rockstar Games
+echo Il gioco è fatto, divertiti!
+
+echo Premi <Invio> per i collegamenti ai download.
+pause >nul
